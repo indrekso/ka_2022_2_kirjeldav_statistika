d3b07dd018870a994e0cd2f91d02e6608d756508
Indrek Soidla <indrek.soidla@ut.ee>
2022-09-17 09:22:38 +0300

    Initial commit

---
+++ ka_2022_2_kirjeldav_statistika.R
@@ -0,0 +1,4 @@
+# Kvantitatiivsete meetodite aluskursus
+# Praktikumi 2 eel: tunnuste ja andmetüübid, kirjeldav statistika
+
+# Siin saate kaasa teha praktikumieelse juhendmaterjali tegevusi, kopeerides juhendist koodijupid või juhendi põhjal need ise sisestades. Kui tekib probleeme või vajate abi, võtke õppejõuga ühendust.
